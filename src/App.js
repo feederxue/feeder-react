e4ca368e3c6f2ebce68ba08323d251d4c5f501aa
axue <1440793825@qq.com>
2019-01-18 19:20:33 +0800

    1

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './common/css/index.css'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        APP
+      </div>
+    );
+  }
+}
+
+export default App;
